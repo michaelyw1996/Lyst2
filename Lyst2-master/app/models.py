60353857481d5a0ffa87a993010d83275f108a57
michaelyw1996 <44041891+michaelyw1996@users.noreply.github.com>
2019-12-03 16:37:56 -0800

    heroku111

---
+++ Lyst2-master/app/models.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from app import db
+from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    todo = db.relationship('Todo', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    text = db.Column(db.String(200))
+    complete = db.Column(db.Boolean)
+    importantItem = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Forum(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+
+
+#putting this
+db.create_all()
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
