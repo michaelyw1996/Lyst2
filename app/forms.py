90a99683181d8b138d9f48e4a0b93e20ca8cfb56
michaelyw1996 <44041891+michaelyw1996@users.noreply.github.com>
2019-12-09 19:39:58 -0800

    small error fix

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from .models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
